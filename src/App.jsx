71b94aeeaddb7a1b5068a996684a3156665d2c9a
giannivitale <giavitale78@gmail.com>
2025-11-12 15:52:34 +0100

    Add bootstrap and give little style

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,40 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect} from 'react';
 import axios from 'axios';
 
 export default function App(){
   const [actresses, setActresses] = useState ([]);
-}
+  
 
 
-function fetcActresses() {
+function fetchActresses() {
   axios.get("https://lanciweb.github.io/demo/api/actresses/")
-  .then((response) => setActresses(response.data) )
+  .then((response) => {
+    
+    setActresses(response.data)
+
+
+  } )
+
+
+}
+  
+//solo al primo rendering ??????
+  useEffect(fetchActresses, []);
+
+
   return (
     <>
-     <ul>
+    <div className='container '>
       <h1>Lista Attrici</h1>
-      
-
-     </ul>
-
-
-
-
-
+      <ul className='list-group'>
+        {actresses.map((actress) => (
+          <li className='list-group-item' key={actress.id}>{actress.name}</li>
+        ))}
+      </ul>
+    </div>
 
 
 
     </>
-  )
+  );
 }
-
